f194a9844843f4f1c94288a5421a56868d5a7657
Jeromé Dolman <jerome@novapsi.net.nz>
2017-04-26 20:09:19 -0400

    Add highlighting of solution candidates by refactoring out a class to represent each candidate in the View Model.

--- SudokuExplorer/BoardViewModel.cs
+++ SudokuExplorer/BoardViewModel.cs
@@ -8,6 +8,51 @@ using System.Threading.Tasks;
 
 namespace SudokuExplorer
 {
+	public class BoardCellCandidate : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		private void SetHelper(ref bool field, bool value, [CallerMemberName] string propertyName = "")
+		{
+			if (value != field)
+			{
+				field = value;
+				NotifyPropertyChanged(propertyName);
+			}
+		}
+
+		private int _label;
+		private bool _isActive;
+		private bool _isSolution;
+
+		public BoardCellCandidate(int label)
+		{
+			_label = label;
+		}
+
+		public string Label
+		{
+			get { return _label.ToString(); }
+		}
+
+		public bool IsActive
+		{
+			get { return _isActive; }
+			set { SetHelper(ref _isActive, value); }
+		}
+
+		public bool IsSolution
+		{
+			get { return _isSolution; }
+			set { SetHelper(ref _isSolution, value); }
+		}
+	}
+
 	public class BoardCell : INotifyPropertyChanged
 	{
 		public event PropertyChangedEventHandler PropertyChanged;
@@ -34,20 +79,15 @@ namespace SudokuExplorer
 
 		private bool _areCandidatesVisible;
 
-		private bool _isCandidate_1;
-		private bool _isCandidate_2;
-		private bool _isCandidate_3;
-		private bool _isCandidate_4;
-		private bool _isCandidate_5;
-		private bool _isCandidate_6;
-		private bool _isCandidate_7;
-		private bool _isCandidate_8;
-		private bool _isCandidate_9;
+		private readonly BoardCellCandidate[] _candidates = new BoardCellCandidate[9];
 
 		public BoardCell(BoardViewModel board, int index)
 		{
 			_board = board;
 			_index = index;
+
+			for (int i = 0; i < 9; i++)
+				_candidates[i] = new BoardCellCandidate(i + 1);
 
 			OnBoardChanged();
 		}
@@ -70,15 +110,10 @@ namespace SudokuExplorer
 			set { SetHelper(ref _areCandidatesVisible, value); }
 		}
 
-		public bool IsCandidate_1 { get { return _isCandidate_1; } set { SetHelper(ref _isCandidate_1, value); } }
-		public bool IsCandidate_2 { get { return _isCandidate_2; } set { SetHelper(ref _isCandidate_2, value); } }
-		public bool IsCandidate_3 { get { return _isCandidate_3; } set { SetHelper(ref _isCandidate_3, value); } }
-		public bool IsCandidate_4 { get { return _isCandidate_4; } set { SetHelper(ref _isCandidate_4, value); } }
-		public bool IsCandidate_5 { get { return _isCandidate_5; } set { SetHelper(ref _isCandidate_5, value); } }
-		public bool IsCandidate_6 { get { return _isCandidate_6; } set { SetHelper(ref _isCandidate_6, value); } }
-		public bool IsCandidate_7 { get { return _isCandidate_7; } set { SetHelper(ref _isCandidate_7, value); } }
-		public bool IsCandidate_8 { get { return _isCandidate_8; } set { SetHelper(ref _isCandidate_8, value); } }
-		public bool IsCandidate_9 { get { return _isCandidate_9; } set { SetHelper(ref _isCandidate_9, value); } }
+		public BoardCellCandidate[] Candidates
+		{
+			get { return _candidates; }
+		}
 
 		internal void OnBoardChanged()
 		{
@@ -189,15 +224,19 @@ namespace SudokuExplorer
 			{
 				BoardCell cell = _data[i];
 				int c = candidates.cellCandidates[i];
-				cell.IsCandidate_1 = (c & (1 << 1)) != 0;
-				cell.IsCandidate_2 = (c & (1 << 2)) != 0;
-				cell.IsCandidate_3 = (c & (1 << 3)) != 0;
-				cell.IsCandidate_4 = (c & (1 << 4)) != 0;
-				cell.IsCandidate_5 = (c & (1 << 5)) != 0;
-				cell.IsCandidate_6 = (c & (1 << 6)) != 0;
-				cell.IsCandidate_7 = (c & (1 << 7)) != 0;
-				cell.IsCandidate_8 = (c & (1 << 8)) != 0;
-				cell.IsCandidate_9 = (c & (1 << 9)) != 0;
+				for (int j = 1; j < 10; j++)
+				{
+					cell.Candidates[j - 1].IsActive = (c & (1 << j)) != 0;
+					cell.Candidates[j - 1].IsSolution = false;
+				}
+			}
+
+			// TODO: Perhaps this should be bound to the UI?
+			// TODO: Stash the reason also to display upon focus
+			var solutions = EliminationSolver.Solve(candidates, true, true);
+			foreach (var solution in solutions)
+			{
+				_data[solution.Ordinal].Candidates[solution.Candidate - 1].IsSolution = true;
 			}
 		}
 
